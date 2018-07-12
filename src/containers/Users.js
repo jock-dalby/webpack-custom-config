68e617304eaadd0ee304c5261aa8719c6de0608c
jock-dalby <jockdalby86@gmail.com>
2018-07-12 18:13:04 +1200

    setup Users container and routing in App.js

---
+++ src/containers/Users.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+  render () {
+    return (
+      <div>
+        <h1>The Users</h1>
+        <ul>
+          <li>Jock</li>
+          <li>Nele</li>
+          <li>Leon</li>
+          <li>Alfie</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Users;
